f0d212218dc69f4158b8ccbef090316a3b166491
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 05:09:40 +0500

    CLI commit

--- main.py
+++ main.py
@@ -18,17 +18,23 @@ async def start(message: types.Message):
 async def respond(callback_query: types.CallbackQuery):
     global page_number
     page_number += 1
-    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+    await bot.delete_message(
+        callback_query.from_user.id, callback_query.message.message_id
+    )
     await bot.send_message(
         callback_query.from_user.id,
         "You are an asshole!",
         reply_markup=teachers_list(page_number),
     )
 
+
 @disp.callback_query_handler(lambda query: str(query.data).startswith("School"))
 async def choice(callback_query: types.CallbackQuery):
     choice = callback_query.data.split(":")
-    await bot.send_message("Your choice: ")
+    await bot.send_message(
+        chat_id=callback_query.from_user.id, text=f"Your choice: {choice[-1]}"
+    )
+
 
 if __name__ == "__main__":
     executor.start_polling(disp, skip_updates=True)
