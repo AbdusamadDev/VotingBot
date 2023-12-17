96bf4ef75b748f869d28661553a91c5cf86afe70
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 05:19:45 +0500

    CLI commit

--- buttons.py
+++ buttons.py
@@ -5,12 +5,15 @@ from utils import get_teachers_name
 
 def teachers_list(page_number):
     teachers_list = [
-        InlineKeyboardButton(text=f"N{i}", callback_data=f"School:{i}")
-        for i in range((page_number * 4) - 4, page_number * 4)
+        [
+            InlineKeyboardButton(text=f"N{i}", callback_data=f"School:{i}")
+            for i in range((page_number * 4) - 4, page_number * 4)
+        ]
+        for _ in range(2)
     ]
     back_button = InlineKeyboardButton(text="<< Avvalgisi", callback_data="back")
     next_button = InlineKeyboardButton(text="Keyingisi >>", callback_data="next")
     buttons = InlineKeyboardMarkup(
-        inline_keyboard=[teachers_list, [back_button, next_button]]
+        inline_keyboard=[*teachers_list, [back_button, next_button]]
     )
     return buttons
