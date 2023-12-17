2e6f49a412146385272bdde128def16bd8d19050
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 23:39:17 +0500

    CLI commit

--- bot/main.py
+++ bot/main.py
@@ -24,8 +24,8 @@ captcha_images = [
         os.path.join(
             "".join(
                 [i + "/" for i in os.path.abspath(__name__).split("\\")[:-2]],
-            ), filename 
-        ), filename.split)
+            ), 
+        ), filename.split(".")[0] for filename in os.listdir()
     )
 ]
 
--- bot/tests.py
+++ bot/tests.py
@@ -1,9 +1,21 @@
-# import os
+import os
 
-# print(
-#     os.path.join(
-#         "".join(
-#             [i + "/" for i in os.path.abspath(__name__).split("\\")[:-2]],
-#         ),
-#     )
-# )
+captcha_images = [
+    (
+        os.path.join(
+            "".join(
+                [i + "/" for i in os.path.abspath(__name__).split("\\")[:-2]],
+            ),
+        ),
+        filename.split(".")[0],
+    )
+    for filename in os.listdir(
+        os.path.join(
+            "".join(
+                [i + "/" for i in os.path.abspath(__name__).split("\\")[:-2]],
+            ),
+            "docs/captcha",
+        )
+    )
+]
+print(captcha_images)
