1ff40a4c729cc03ad6fc13d49ab6a8d35bf4d9ff
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 18:54:27 +0500

    CLI commit

--- main.py
+++ main.py
@@ -80,19 +80,43 @@ async def choice(callback_query: types.CallbackQuery, state: FSMContext):
 @disp.callback_query_handler(lambda query: query.data == "subscribed")
 async def subscribtion_handler(callback_query: types.CallbackQuery):
     subscribtion_click[callback_query.from_user.id] = callback_query.data.split(":")[1]
-    print(subscribtion_click)
+    print("List of channels are: ", await get_channels_list(callback_query.from_user.id))
     await bot.send_message(
         text="Captchadan uting: manu nichchi 2255?", chat_id=callback_query.from_user.id
     )
     await VotingState.captcha.set()
 
 
-@disp.message_handler(state=VotingState.captcha)
-async def captcha_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    print(f"Current state: {current_state}")
-    await state.finish()
-    await message.answer("Thank you for your response!")
+async def get_channels_list(user_id):
+    # Get a list of updates (new messages)
+    updates = await bot.get_updates(limit=100)
+
+    # Iterate through updates to find channel information
+    channels = []
+    for update in updates:
+        # Check if the update has a message
+        if update.message:
+            # Get the chat ID
+            chat_id = update.message.chat.id
+            # Check if the user is a member of the chat (channel)
+            member = await bot.get_chat_member(chat_id, user_id)
+
+            # Check if the user is a member of the channel
+            if member.status in (
+                types.ChatMemberStatus.ADMINISTRATOR,
+                types.ChatMemberStatus.MEMBER,
+            ):
+                channels.append(update.message.chat)
+
+    return channels
+
+
+# @disp.message_handler(state=VotingState.captcha)
+# async def captcha_handler(message: types.Message, state: FSMContext):
+#     current_state = await state.get_state()
+#     print(f"Current state: {current_state}")
+#     await state.finish()
+#     await message.answer("Thank you for your response!")
 
 
 CHANNEL_USERNAME = "@LAYFXAK_KANAL"
