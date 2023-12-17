b87affc0c4fffa33ff6376c80366643dcd9bd3e8
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 20:55:30 +0500

    CLI commit

--- database.py
+++ database.py
@@ -24,6 +24,8 @@ class Database:
             (id INTEGER, fullname TEXT, school TEXT, number_of_votes INTEGER, 
             PRIMARY KEY ('id'))""",
         )
+        for details in get_teachers_name():
+            
         self.connection.commit()
 
     def add_user(self, username, first_name, telegram_id):
@@ -35,12 +37,24 @@ class Database:
         )
         self.connection.commit()
 
-    def voting(self, telegram_id, boolean):
+    def voting(self, telegram_id, boolean, school):
         self.cursor.execute(
             """UPDATE users SET is_voted = ? WHERE telegram_id = ?;""",
             (boolean, telegram_id),
         )
-        self.cursor.execute(
-            """UPDATE teachers SET number_of_votes = ?""",
+        number_of_votes = self.cursor.execute(
+            """SELECT number_of_votes FROM teachers WHERE school LIKE ?""",
+            (school,),
         )
+        if number_of_votes:
+            number_of_votes = number_of_votes.fetchone()
+        # self.cursor.execute(
+        #     """UPDATE teachers SET number_of_votes = ? WHERE school = ?""", ()
+        # )
+        print(number_of_votes)
         self.connection.commit()
+
+
+if __name__ == "__main__":
+    database = Database()
+    database.voting(2003049919, 1, "1 - maktab")
