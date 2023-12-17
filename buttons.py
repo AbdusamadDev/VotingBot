4724c9dcf3cb9071a7c009283b1cfa818bf8e113
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 06:05:07 +0500

    CLI commit

--- buttons.py
+++ buttons.py
@@ -1,5 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from utils import get_teachers_name
+
 
 def teachers_list(page_number):
     teachers_list = []
@@ -7,9 +9,13 @@ def teachers_list(page_number):
         print("K: ", k)
         print("Page number: ", page_number * 8)
         inner_list = []
-        print("Rough Calculation: ", k * 4)
-        print(f"From {(page_number * 8) - k * 4} To {page_number * 4 * k}")
-        for i in range((page_number * 8) - k * 4, page_number * 4 * k):
+        print("Rough Calculation: ", [1, 2][::-1][k - 1] * 4)
+        print(
+            f"From {(page_number * 8) - [1, 2][::-1][k - 1] * 4} To {page_number * (k * 4)}"
+        )
+        for i in range(
+            (page_number * 8) - [1, 2][::-1][k - 1] * 4, (page_number * 4) * k
+        ):
             inner_list.append(
                 InlineKeyboardButton(text=f"N{i}", callback_data=f"School:{i}")
             )
@@ -21,5 +27,5 @@ def teachers_list(page_number):
         inline_keyboard=[*teachers_list, [back_button, next_button]]
     )
     print(teachers_list)
-    print("\n\n")
+    print("\n\n")   
     return buttons
