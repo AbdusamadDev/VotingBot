86ae2964e51766a2b5e7f0b001026af184e2efc7
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-18 00:49:05 +0500

    CLI commit

--- bot/buttons.py
+++ bot/buttons.py
@@ -7,7 +7,7 @@ def teachers_list(start_page, end_page, labels):
     print(labels)
     teachers_list = lambda limit: [
         InlineKeyboardButton(text=labels[i], callback_data=f"School:{labels[i]}")
-        for i in range(*limit)
+        for i in range(*limit) if limit >= 8 else InlineKeyboardButton(labels) 
     ]
     back_button = InlineKeyboardButton(text="<< Avvalgisi", callback_data="back")
     next_button = InlineKeyboardButton(text="Keyingisi >>", callback_data="next")
@@ -23,12 +23,26 @@ def teachers_list(start_page, end_page, labels):
     )
     return buttons
 
-
-def get_users(usernames):
-    buttons = [
-        InlineKeyboardButton(text=username, callback_data=f"user:{username}")
-        for username in usernames
+def teachers_list(start_page, end_page, labels):
+    part_1 = (start_page, start_page + 4)
+    part_2 = (start_page + 4, end_page)
+    print(labels)
+    teachers_list = lambda limit: [
+        InlineKeyboardButton(text=labels[i], callback_data=f"School:{labels[i]}")
+        for i in range(*limit)
     ]
+    back_button = InlineKeyboardButton(text="<< Avvalgisi", callback_data="back")
+    next_button = InlineKeyboardButton(text="Keyingisi >>", callback_data="next")
+    buttons = InlineKeyboardMarkup(
+        inline_keyboard=[
+            teachers_list(part_1),
+            teachers_list(part_2),
+            [
+                back_button if len(labels) >= 0 else [],
+                next_button if len(labels) >= end_page else [],
+            ],
+        ]
+    )
     return buttons
 
 
