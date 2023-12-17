88f0816e85caceac2cb364a6c93015dfc5699fd0
AbdusamadDev <abdusamaddev571@gmail.com>
2023-12-17 19:49:55 +0500

    CLI commit

--- database.py
+++ database.py
@@ -3,15 +3,28 @@ import sqlite3
 
 class Database:
     def __init__(self, name: str = "db.sqlite3") -> None:
-        seconnection = sqlite3.connect(name)
-        self.cursor = connection.cursor()
+        self.connection = sqlite3.connect(name)
+        self.cursor = self.connection.cursor()
+        self.create_users_table()
 
     def create_users_table(self):
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS users 
             (id INTEGER, username TEXT, first_name TEXT, telegram_id INTEGER,
-            PRIMARY KEY ('id'))
+            is_voted INTEGER, PRIMARY KEY ('id'))
             """
         )
-        
+        self.connection.commit()
+
+    def add_user(self, username, first_name, telegram_id):
+        self.cursor.execute(
+            """INSERT INTO users 
+            (username, first_name, telegram_id, is_voted) 
+            VALUES (?, ?, ?, ?)""",
+            (username, first_name, telegram_id, 0),
+        )
+        self.connection.commit()
+
+    def voting(self):
+        self.cursor.execute("""""")
